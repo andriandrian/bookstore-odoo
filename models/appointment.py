5155110f3d2dcb10e9793c2dda6391ef5cd1befd
andriandrian <naird.id@gmail.com>
2023-04-05 11:04:25 +0700

    25. Define Onchange Functions In Odoo

--- models/appointment.py
+++ models/appointment.py
@@ -9,3 +9,8 @@ class BookstoreAppointment(models.Model):
     gender = fields.Selection(string="Gender", related='employee_id.gender')
     appointment_time = fields.Datetime(string='Appointment Time', default=fields.Datetime.now)
     booking_date = fields.Date(string='Booking Date', default=fields.Date.context_today)
+    ref = fields.Char(string='Reference', default='New Reference')
+
+    @api.onchange('employee_id')
+    def onchange_employee_id(self):
+        self.ref = self.employee_id.ref
