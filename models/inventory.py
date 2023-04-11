9d1c2e9dd97991c11d435560cd5e26ef6b32b471
andriandrian <naird.id@gmail.com>
2023-04-11 11:04:39 +0700

    Add status bar in Inventory Form

--- models/inventory.py
+++ models/inventory.py
@@ -14,21 +14,25 @@ class Inventory(models.Model):
         string='Invoice Date', default=fields.Datetime.now, tracking=True)
     # ref = fields.Char(string="Reference", related='bookstore_book.ref')
     ref = fields.Char(string='Reference', readonly=True, related='name.ref')
-    serial_number = fields.Char(string='Lot/Serial Number', tracking=True)
-    vendor = fields.Char(string='From', tracking=True)
-    to = fields.Char(string='To', tracking=True)
-    stock = fields.Integer(string='Stock', tracking=True)
-    total_qty = fields.Integer(string='Balance', tracking=True)
+    # vendor = fields.Char(string='From', tracking=True)
+    # to = fields.Char(string='To', tracking=True)
+    stock = fields.Integer(string='Stock', tracking=True, required=True)
+    # total_qty = fields.Integer(string='Balance', tracking=True)
     unit_measure = fields.Selection([('piece', 'PCs'), ('dozen', 'Dozen')],
                                     string="Unit of Measure", default='piece', tracking=True, required=True)
-    company = fields.Many2one('res.company', string='Company', tracking=True)
+    company = fields.Many2one(
+        'res.company', string='To', tracking=True, required=True)
     state = fields.Selection([('draft', 'Draft'), ('done', 'Done')],
                              string="State", default='draft', tracking=True, required=True)
     stock_type = fields.Selection(
         [('in', 'In'),
          ('out', 'Out')],
         string="Type", default='in', tracking=True, required=True)
+    state = fields.Selection([('draft', 'Draft'),
+                              ('shipped', 'Shipped'),
+                              ('completed', 'Completed'),
+                              ('cancel', 'Cancelled')], string="State", default='draft', tracking=True, required=True)
 
-    @api.onchange('book_id')
+    @ api.onchange('book_id')
     def onchange_book_id(self):
         self.ref = self.book_id.ref
